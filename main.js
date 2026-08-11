7a1b108cd46e9a6074640cde30611b470ea0f61e
DmitryBloomberg <213293116+DmitryBloomberg@users.noreply.github.com>
2026-08-11 19:01:41 +0300

    1

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // ===== Подключение к PostgreSQL =====
 const pool = new Pool({
-  host: 'http://13.143.64.167',
+  host: '13.143.64.167',
   port: 6432,
   database: 'KomfortDatabase',
   user: 'postgres',
